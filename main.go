e7a369a32077568c091d4ba43b0a8887829d6c62
Steve Lacy <me@slacy.me>
2021-08-30 13:42:53 -0700

    output version on run

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/fatih/color"
 	"github.com/stevelacy/monokube/pkg/monokube"
 )
 
+var Version string
+
 func main() {
+	color.Cyan("monokube version: %s\n", Version)
 	monokube.Init()
 }
